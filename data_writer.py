177adfc1bb32f21b5776fb589bf845cd659d7331
Maarten <maarten7post@gmail.com>
2018-05-23 14:35:03 +0200

    data_writer and trainer for memory control

--- data_writer.py
+++ data_writer.py
@@ -17,37 +17,27 @@ Data_handle = model.Data_handle
 EventFile.read_timeslices = True
 def data_writer(title):
     dh = Data_handle() 
-    with h5py.File(title, "w") as hfile:
-        tbins = []
+    with h5py.File(title, "a") as hfile:
         for root_file, evt_type in root_files(train=True, test=True):
             print root_file 
             f = EventFile(root_file)
-        #    f.use_tree_index_for_mc_reading = True
+            f.use_tree_index_for_mc_reading = True
             
+            shape = (len(f), 50, 13, 13, 18, 1)
+            dset_e = hfile.create_dataset(root_file, dtype='float64', shape=shape)
+            shape = (len(f), 3)
+            dset_l = hfile.create_dataset(root_file + "labels", dtype='int64', shape=shape)        
             ####################################################    
-        #    events = np.empty((0, 140, 13, 13, 18, 31))
-        #    labels = np.empty((0, 3))
             
-            for evt in f:
-                hits = evt.hits
-                num_tbins = dh.make_event(hits)
-                tbins.append(num_tbins)
-        
-            print max(tbins), min(tbins)
+            for i, evt in enumerate(f):
+                event = dh.make_event(evt.hits)
+                try:
+                    label = dh.make_labels(evt_type)
+                except TypeError:
+                    label = dh.make_label(E, dx, dy, dz)
 
-
-#
-#                try:
-#                    label = dh.make_labels(evt_type)
-#                except TypeError:
-#                    label = dh.make_label(E, dx, dy, dz)
-#
-#                
-#                events = np.append(events, [event], axis=0)
-#                labels = np.append(labels, [label], axis=0)
-#                    
-#            dset = hfile.create_dataset(root_file, data=events, dtype='float64')
-#            dset = hfile.create_dataset(root_file + "labels", data=labels, dtype='int64')        
+            dset_e[i] = event     
+            dset_l[i] = label 
             ####################################################
                 
 
